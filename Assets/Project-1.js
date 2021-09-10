cdb2db39d29c753824d49fa4b18b0aff7d042077
Henry Deserne <Hdeserne@gmail.com>
2021-09-10 12:42:42 -0700

    commit

--- Assets/Project-1.js
+++ Assets/Project-1.js
@@ -1,1 +1,22 @@
-console.log("Hello World")
+var Veggie = document.getElementsByClassName("Veggie Meals");
+
+Veggie.addEventListener("click", function()){
+
+}
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("Veggie Meals");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
